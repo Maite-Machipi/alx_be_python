7071119221f29a099059446c887eaa6434b592c3
Maite Marageni <mmarageni@sansa.org.za>
2025-11-07 09:31:02 +0200

    Create rectangle_area.py

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,12 @@
+#rectangle_are.py
+#Program to calculate the area of the rectangle
+
+#Define variables
+length = 10 #length of the rectangle
+width = 5 # width of the rectangle
+
+#calculate area
+area = length * width
+
+#print the result
+print(f"The area of the rectangle is: {area}")
