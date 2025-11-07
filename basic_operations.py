baa373e5b68c54454372544e695985d945255700
Maite <maitemarageni@gmail.com>
2025-11-07 08:52:26 +0200

    Added basic_operations.py with sum, difference and product calculations

---
+++ basic_operations.py
@@ -0,0 +1,15 @@
+#Define the  variables
+
+number1 = 11
+number2 = 9
+
+
+#Perform calculations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+#Print results
+print(f"sum of {number1} and {number2} is {sum_result}")
+print(f"Difference of {number1} and {number2} is {difference_result}")
+print(f"Product of {number1} and {number2} is {product_result}")
